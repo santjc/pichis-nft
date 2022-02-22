93dba94ac319dd01df7fadee1641002c98c08507
santjc <sjcolombatto@hotmail.com>
2022-02-22 20:33:37 -0300

    Hardhat dependencies

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Characters = await hre.ethers.getContractFactory("Characters");
+  const character = await Characters.deploy();
 
-  await greeter.deployed();
+  await character.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Character deployed to:", character.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
